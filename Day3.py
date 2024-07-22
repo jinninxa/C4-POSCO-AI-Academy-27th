87d1cc1a973b98de4d351769011fcc93c0aa8cf7
jaeheon2673 <posco2673@gmail.com>
2024-07-22 21:01:59 +0900

    재헌 테스트

---
+++ Day3.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+print("Hello world")
+
+
+# In[4]:
+
+
+print("Hello, WOrld")
+
+
+# # Hello WORLD 출력
+# + Print 함수로 ***Hello, World***를
+# + 출력합니다. 
+
+# In[3]:
+
+
+print("Hello, world!!")
+print("hhh")
+
+
+# In[4]:
+
+
+print("Hello, World")
+
+
+# In[6]:
+
+
+a = int(input("첫 번째 정수를 입력하세요 :"))
+b = int(input("두 번째 정수를 입력하세요 :"))
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
